2af71f850393dc9e28d3bccf3b29b2acce35b2e3
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-02 21:02:14 +0500

    Utility items : Application : Create Common/Utility/SD.cs : static class and inside roles (Admin, Chef);

---
+++ RecipeSharingPlatform.Application/Common/Utility/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeSharingPlatform.Application.Common.Utility
+{
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Chef = "Chef";
+    }
+}
