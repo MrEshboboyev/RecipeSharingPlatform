f3cab6b531aa2efe239a378c36423e499013db8c
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-10 23:49:15 +0500

    Presentation : Configure JwtAuthentication in Program.cs; Adding package : Microsoft.AspNetCore.Authentication.JwtBearer;

--- RecipeSharingPlatform.Presentation/Program.cs
+++ RecipeSharingPlatform.Presentation/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using RecipeSharingPlatform.Application.Services.Interfaces;
 using RecipeSharingPlatform.Domain.Entities;
 using RecipeSharingPlatform.Infrastructure.Data;
 using RecipeSharingPlatform.Infrastructure.Implementations;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +20,27 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
+
+// configure JwtAuthentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtSettings:Key"]);
+var issuer = builder.Configuration["JwtSettings:Issuer"];
+var audience = builder.Configuration["JwtSettings:Audience"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience
+    };
+});
 
 // adding lifetime for services
 builder.Services.AddScoped<IAuthService, AuthService>();
