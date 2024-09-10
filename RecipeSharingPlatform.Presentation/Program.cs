3adb3d5676ed34ffd64c345821ce4a083e99f844
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-10 23:50:47 +0500

    Add swagger authentication;

--- RecipeSharingPlatform.Presentation/Program.cs
+++ RecipeSharingPlatform.Presentation/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RecipeSharingPlatform.Application.Services.Interfaces;
 using RecipeSharingPlatform.Domain.Entities;
 using RecipeSharingPlatform.Infrastructure.Data;
@@ -14,7 +15,38 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    option =>
+    {
+        option.AddSecurityDefinition
+        (
+            name: JwtBearerDefaults.AuthenticationScheme,
+            securityScheme: new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = "Enter the Bearer Authorization string as following : `Bearer Generated-JWT-Token`",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            }
+        );
+        option.AddSecurityRequirement
+        (
+            new OpenApiSecurityRequirement
+            {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = JwtBearerDefaults.AuthenticationScheme
+                    }
+                }, new string [] { }
+            }
+            }
+        );
+    });
 
 // adding identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
