f46b9df36b3c0f0194fe70fd3c2e750527bc2ae6
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-11 18:50:11 +0500

    Unit Of Work INTERFACE AND IMPLEMENTATION : Application : Create Common/Interfaces/IUnitOfWork.cs : Save(); Infrastructure : Repositories/UnitOfWork.cs : inherit from IUnitOfWork, inject db, method implemented;

---
+++ RecipeSharingPlatform.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace RecipeSharingPlatform.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        void Save();
+    }
+}
---
+++ RecipeSharingPlatform.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using RecipeSharingPlatform.Application.Common.Interfaces;
+using RecipeSharingPlatform.Infrastructure.Data;
+
+namespace RecipeSharingPlatform.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _db;
+
+        public UnitOfWork(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
