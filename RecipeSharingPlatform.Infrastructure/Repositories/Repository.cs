999b0340a8837ffbb6f606cfb71753f375324f06
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-11 18:46:59 +0500

    GENERIC REPO INTERFACE AND IMPLEMENTATION : Application : Create Common/Interfaces/IRepository.cs : CRUD for Generic; Infrastructure : Create Repositories/Repository.cs : inject DbContext and DbSet, inherit from IRepository with generic, methods implemented;

---
+++ RecipeSharingPlatform.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeSharingPlatform.Application.Common.Interfaces;
+using RecipeSharingPlatform.Infrastructure.Data;
+using System.Linq.Expressions;
+
+namespace RecipeSharingPlatform.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        // inject DI, internal DbSet<T>
+        private readonly AppDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(AppDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public bool Any(Expression<Func<T, bool>> filter)
+        {
+            return dbSet.Any(filter);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        {
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrWhiteSpace(includeProperties))
+            {
+                foreach (var property in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property.Trim());
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false)
+        {
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrWhiteSpace(includeProperties))
+            {
+                foreach (var property in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property.Trim());
+                }
+            }
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+    }
+}
