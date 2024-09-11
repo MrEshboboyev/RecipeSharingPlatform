5abd52b21d1d9a6e8f39b15c19d44bb4f51dca82
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-11 20:54:03 +0500

    Recipe Label interface writed : Application : Create Services/Interfaces/IRecipeLabelService.cs : CRUD and Label Recipes;

---
+++ RecipeSharingPlatform.Application/Services/Interfaces/IRecipeLabelService.cs
@@ -0,0 +1,14 @@
+﻿using RecipeSharingPlatform.Domain.Entities;
+
+namespace RecipeSharingPlatform.Application.Services.Interfaces
+{
+    public interface IRecipeLabelService
+    {
+        Task<IEnumerable<RecipeLabel>> GetAllLabelsAsync();
+        Task<IEnumerable<Recipe>> GetLabelRecipesAsync(Guid labelId);
+        Task<RecipeLabel> GetLabelByIdAsync(Guid labelId);
+        Task CreateLabelAsync(RecipeLabel recipeLabel);  
+        Task UpdateLabelAsync(RecipeLabel recipeLabel);  
+        Task DeleteLabelAsync(Guid labelId);  
+    }
+}
