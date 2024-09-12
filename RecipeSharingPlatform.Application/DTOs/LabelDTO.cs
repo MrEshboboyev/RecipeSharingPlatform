5ee48a1d2c8e7c8b10b5dcafd1b9d30935c65b1b
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-12 22:46:14 +0500

    Create Mapping Profile : Application : Create Mappings/MappingProfile.cs : inherit from AutoMapper.Profile, constructor configure(Recipe to RecipeDTO, RecipeLabel, LabelDTO); Adding Package : AutoMapper;

--- RecipeSharingPlatform.Application/DTOs/LabelDTO.cs
+++ RecipeSharingPlatform.Application/DTOs/LabelDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace RecipeSharingPlatform.Application.DTOs
 {
-    public class LabelDto
+    public class LabelDTO
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
---
+++ RecipeSharingPlatform.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RecipeSharingPlatform.Application.DTOs;
+using RecipeSharingPlatform.Domain.Entities;
+
+namespace RecipeSharingPlatform.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Recipe, RecipeDTO>();
+            CreateMap<RecipeLabel, LabelDTO>();
+        }
+    }
+}
