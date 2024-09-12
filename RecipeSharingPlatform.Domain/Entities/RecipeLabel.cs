a4ced855b51b2d5cbbd7b6ce0e7f2394d560a0cc
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-12 16:52:53 +0500

    Added JsonIgnore attribute to Label.Recipes field Domain : Entities/RecipeLabel.cs : added [JsonIgnore] to Recipes;

--- RecipeSharingPlatform.Domain/Entities/RecipeLabel.cs
+++ RecipeSharingPlatform.Domain/Entities/RecipeLabel.cs
@@ -1,9 +1,12 @@
-﻿namespace RecipeSharingPlatform.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace RecipeSharingPlatform.Domain.Entities
 {
     public class RecipeLabel
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        [JsonIgnore]
         public ICollection<Recipe> Recipes { get; set; }
     }
 }
