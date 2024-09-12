c876178341846e71386363bf3de626406bb0d8de
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-12 16:13:51 +0500

    Add Label to Recipe method in Recipe Repo : Application : Common/Interfaces/IRecipeRepository.cs : add method [AddLabelToRecipeAsync];

--- RecipeSharingPlatform.Application/Common/Interfaces/IRecipeRepository.cs
+++ RecipeSharingPlatform.Application/Common/Interfaces/IRecipeRepository.cs
@@ -5,5 +5,6 @@ namespace RecipeSharingPlatform.Application.Common.Interfaces
     public interface IRecipeRepository : IRepository<Recipe>
     {
         void Update(Recipe recipe);
+        Task AddLabelToRecipeAsync(Guid recipeId, Guid labelId);
     }
 }
