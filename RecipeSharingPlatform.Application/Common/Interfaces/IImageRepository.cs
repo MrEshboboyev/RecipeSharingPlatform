48b8453adc5981a6c7e92aaab69347f3acfcab8c
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-13 21:43:20 +0500

    Create Image Repo : Application : Create Common/Interfaces/IImageRepository.cs : GetImageAsync(), FileExists();

---
+++ RecipeSharingPlatform.Application/Common/Interfaces/IImageRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeSharingPlatform.Application.Common.Interfaces
+{
+    public interface IImageRepository
+    {
+        Task<byte[]> GetImageAsync(string filePath);
+        bool FileExists(string filePath);
+    }
+}
