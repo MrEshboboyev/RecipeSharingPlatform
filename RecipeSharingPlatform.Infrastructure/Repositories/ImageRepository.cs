587c6cbae81ae5f6a97cd40bf35f677d644ac306
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-13 21:47:06 +0500

    Create Image Repo implementation : Infrastructure : Create Repositories/ImageRepository.cs : methods implemented;

---
+++ RecipeSharingPlatform.Infrastructure/Repositories/ImageRepository.cs
@@ -0,0 +1,17 @@
+﻿using RecipeSharingPlatform.Application.Common.Interfaces;
+
+namespace RecipeSharingPlatform.Infrastructure.Repositories
+{
+    public class ImageRepository : IImageRepository
+    {
+        public bool FileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        public Task<byte[]> GetImageAsync(string filePath)
+        {
+            return File.ReadAllBytesAsync(filePath);
+        }
+    }
+}
