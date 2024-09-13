6f6088c8f6547d6b56325d9fbc9b044683deb8e3
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-13 21:55:18 +0500

    Create Image Service implementation : Infrastructure : Create Implementations/ImageService.cs : inherit from IImageService, inject IImageRepository, _imageFolderPath, method implemented;

---
+++ RecipeSharingPlatform.Infrastructure/Implementations/ImageService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using RecipeSharingPlatform.Application.Common.Interfaces;
+using RecipeSharingPlatform.Application.Services.Interfaces;
+
+namespace RecipeSharingPlatform.Infrastructure.Implementations
+{
+    public class ImageService : IImageService
+    {
+        private readonly IImageRepository _imageRepository;
+        private readonly string _imageFolderPath;
+
+        public ImageService(IConfiguration configuration,
+            IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+            _imageFolderPath = configuration["ImageSettings:ImageFolderPath"] ?? Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+        }
+
+        public async Task<byte[]> GetImageAsync(string fileName)
+        {
+            try
+            {
+                var filePath = Path.Combine(_imageFolderPath, fileName);
+
+                if (!_imageRepository.FileExists(filePath))
+                    throw new Exception("Image not found!");
+
+                return await _imageRepository.GetImageAsync(filePath);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
