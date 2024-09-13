84f520911b4b0c36f34dae96233bcc0d1fa94b7c
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-13 13:06:37 +0500

    Recipe filter model writed : Application : Create Common/Filters/RecipeFilterParam.cs : Keyword, StarDate, EndDate, ChefId, LabelIds;

---
+++ RecipeSharingPlatform.Application/Common/Filters/RecipeFilterParam.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeSharingPlatform.Application.Common.Filters
+{
+    public class RecipeFilterParam
+    {
+        public string Keyword { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Guid? ChefId { get; set; }
+        public List<Guid> LabelIds { get; set; }
+    }
+}
